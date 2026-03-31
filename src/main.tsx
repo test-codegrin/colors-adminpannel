4c4957b3a2856fd40c1b18a04b3a52606088d6fe
Ambaliya Dixit <154045627+Dixitambaliya@users.noreply.github.com>
2026-03-31 23:30:41 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { HeroUIProvider, ToastProvider } from "@heroui/react";
+import { ToastProvider } from "@heroui/react";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
@@ -9,15 +9,14 @@ import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <HeroUIProvider>
-    <ToastProvider placement="bottom-right" />
-    <BrowserRouter>
-      <Provider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </Provider>
-    </BrowserRouter>
-  </HeroUIProvider>,
+  <BrowserRouter>
+    <Provider>
+      <ToastProvider placement="bottom-right" />
+
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
+  </BrowserRouter>,
   // </React.StrictMode>,
 );
